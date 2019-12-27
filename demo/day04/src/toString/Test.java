07bae8964eb4e44c974fc8e1cc5aed242cfa1194
root <932598683@qq.com>
2019-12-27 10:37:19 +0800

    提交项目

---
+++ demo/day04/src/toString/Test.java
@@ -0,0 +1,17 @@
+package toString;
+
+import java.sql.SQLOutput;
+
+public class Test {
+    public static void main(String[] args) {
+        int num = 68 ;
+        char c = (char) num ;
+        System.out.println(c) ;
+
+        byte a = 127;
+
+    }
+
+
+
+}
